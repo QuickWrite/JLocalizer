d74a0c6df380436fa77143566ed7869dfab286fc
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-01-05 12:13:11 +0100

    Add base files for plural rules
    Add the files from which the information is getting generated.

--- annotation-processor/src/main/java/net/quickwrite/localizer/processor/PluralRuleProcessor.java
+++ annotation-processor/src/main/java/net/quickwrite/localizer/processor/PluralRuleProcessor.java
@@ -1,11 +1,18 @@
 package net.quickwrite.localizer.processor;
 
 import com.google.auto.service.AutoService;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Set;
 
 @SupportedAnnotationTypes("net.quickwrite.localizer.processor.PluralRuleGen")
@@ -13,8 +20,31 @@ import java.util.Set;
 @AutoService(Processor.class)
 public class PluralRuleProcessor extends AbstractProcessor {
     @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "This is a simple test " + annotations);
+    public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
+
+
+        final Document document;
+        try {
+            document = getXMLDocument("/plurals.xml");
+        } catch (final ParserConfigurationException | IOException | SAXException e) {
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                    "An exception occurred while trying to parse the input file for @PluralRuleGen -> \n" +
+                            Arrays.toString(e.getStackTrace())
+            );
+
+            return false;
+        }
+
         return true;
+    }
+
+    private File getInputFile(final String path) throws NullPointerException {
+        return new File(Objects.requireNonNull(this.getClass().getResource(path)).getFile());
+    }
+
+    private Document getXMLDocument(final String path)
+            throws ParserConfigurationException, IOException, SAXException, NullPointerException {
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newDefaultInstance();
+        return dbf.newDocumentBuilder().parse(getInputFile(path));
     }
 }
