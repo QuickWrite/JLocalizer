c48ef7114d22930e746e8776f1f6221088667c23
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-01-07 16:26:50 +0100

    Basic parsing and outputting

--- annotation-processor/src/main/java/net/quickwrite/localizer/processor/PluralRuleProcessor.java
+++ annotation-processor/src/main/java/net/quickwrite/localizer/processor/PluralRuleProcessor.java
@@ -18,6 +18,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -63,6 +64,8 @@ public class PluralRuleProcessor extends AbstractProcessor {
                 final Token token = lexer.tokenize(children.item(j).getTextContent());
 
                 // TODO: do something with the token
+
+                unwrapConditions(token.allByName("and_condition"));
             }
         }
 
@@ -77,5 +80,88 @@ public class PluralRuleProcessor extends AbstractProcessor {
             throws ParserConfigurationException, IOException, SAXException, NullPointerException {
         final DocumentBuilderFactory dbf = DocumentBuilderFactory.newDefaultInstance();
         return dbf.newDocumentBuilder().parse(getInputFile(path));
+    }
+
+    private void unwrapConditions(final List<Token> tokens) {
+        final StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < tokens.size(); i++) {
+            if (i != 0) {
+                builder.append(" || ");
+            }
+
+            List<Token> inRelationTokens = tokens.get(i).allByName("in_relation");
+            for (int j = 0; j < inRelationTokens.size(); j++) {
+                if (j != 0) {
+                    builder.append(" && ");
+                }
+
+                unwrapInRelation(inRelationTokens.get(j), builder);
+            }
+        }
+    }
+
+    private void unwrapInRelation(final Token token, final StringBuilder builder) {
+        unwrapExpr(token.getChildren()[0], builder);
+        builder.append(" ");
+
+        final String value = token.getChildren()[1].getValue();
+        if (value.equals("=")) {
+            builder.append("=");
+        }
+        builder.append(value);
+        builder.append(" ");
+        unwrapRangeList(token.getChildren()[2], builder);
+    }
+
+    private void unwrapRangeList(final Token token, final StringBuilder builder) {
+        assert !token.getType().getName().equals("range_list");
+
+        final Token[] tokens = token.getChildren();
+        final Token firstToken = tokens[0].getChildren()[0];
+        if (firstToken.getType().getName().equals("value")) {
+            builder.append(firstToken.getValue());
+        } else {
+            unwrapRange(firstToken, builder);
+        }
+
+        if (tokens.length == 1) {
+            return;
+        }
+
+        //builder.append(" ");
+        for (Token token1 : token.getChildren()[1].getChildren()) {
+            builder.append(",");
+            final Token moreToken = token1.getChildren()[1].getChildren()[0];
+            if (moreToken.getType().getName().equals("value")) {
+                builder.append(moreToken.getValue());
+                continue;
+            }
+
+            unwrapRange(moreToken, builder);
+        }
+    }
+
+    private void unwrapExpr(final Token token, final StringBuilder builder) {
+        assert !token.getType().getName().equals("expr");
+
+        final Token[] children = token.getChildren();
+
+        builder.append("operand.");
+        builder.append(children[0].getValue());
+        builder.append("()");
+
+        if (!(children.length > 1)) {
+            return;
+        }
+
+        builder.append(" % ");
+        builder.append(children[1].allByName("value").get(0).getValue());
+    }
+
+    private void unwrapRange(final Token token, final StringBuilder builder) {
+        assert !token.getType().getName().equals("range");
+        builder.append(token.getChildren()[0].getValue());
+        builder.append(" to ");
+        builder.append(token.getChildren()[2].getValue());
     }
 }
